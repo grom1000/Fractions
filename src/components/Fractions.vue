5785cd02a7379f56ca85e14f61940f32ca33a1d5
Nikolay <ntamarin27@gmail.com>
2021-01-20 17:05:43 +0300

    fractions

--- src/components/Fractions.vue
+++ src/components/Fractions.vue
@@ -3,76 +3,152 @@
     <div class="expression">
       <div class="fraction">
         <input
-          v-model="leftNumerator"
+          v-model="$v.leftNumerator.$model"
           type="text"
           maxlength="2"
           class="number number-numerator"
-          @input="handleValidation(leftNumerator)"
-        >
+          @input="validation($v.leftNumerator.$model)"
+        />
         <span class="fraction-border" />
         <input
-          v-model="leftDenominator"
+          v-model="$v.leftDenominator.$model"
           type="text"
           maxlength="2"
           class="number number-denomunator"
-          @input="handleValidation(leftDenominator)"
-        >
+          @input="validation($v.leftNumerator.$model)"
+        />
       </div>
       <div class="plus alt" />
       <div class="fraction">
         <input
-          v-model="rightNumerator"
+          v-model="$v.rightNumerator.$model"
           type="text"
           maxlength="2"
           class="number number-numerator"
-          @input="handleValidation(rightNumerator)"
-        >
+          @input="validation($v.leftNumerator.$model)"
+        />
         <span class="fraction-border" />
         <input
-          v-model="rightDenominator"
+          v-model="$v.rightDenominator.$model"
           maxlength="2"
           type="text"
           class="number number-denomunator"
-          @input="handleValidation(rightDenominator)"
-        >
+          @input="validation($v.leftNumerator.$model)"
+        />
       </div>
-      <img src="../assets/images/equal.png" alt="Знак равенства" class="equally">
+      <img
+        src="../assets/images/equal.png"
+        alt="Знак равенства"
+        class="equally"
+      />
       <div v-show="!isVisible">
         {{ sum }}
       </div>
+      <div v-for="(item, index) in fractions" :key="index" class="expression">
+        <div class="plus alt" />
+        <div class="fraction">
+          <span @click="deleteFractions(index)" class="delete">&#10006;</span>
+          <div>
+            {{ item }}
+          </div>
+        </div>
+      </div>
+      <div v-if="isVisibleTotal" class="expression">
+        <img
+          src="../assets/images/equal.png"
+          alt="Знак равенства"
+          class="equally"
+        />
+        <div>{{ totalSum }}</div>
+      </div>
     </div>
-    <div v-if="isVisible">
-      Введите целое число от 1 до 99
+    <div class="content">
+      <div v-if="isVisible" class="error-message">
+        Введите целое число от 1 до 99
+      </div>
+      <button class="button" @click="addElement(1, 99)">
+        add new element
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import { maxLength, between, numeric } from "vuelidate/lib/validators";
+
 export default {
-  name: 'Fractions',
+  name: "Fractions",
   data() {
     return {
-      leftNumerator: '',
-      leftDenominator: '',
-      rightNumerator: '',
-      rightDenominator: '',
-      isVisible: false
+      leftNumerator: "",
+      leftDenominator: "",
+      rightNumerator: "",
+      rightDenominator: "",
+      isVisible: false,
+      isVisibleTotal: false,
+      fractions: [],
+      numbersAmount: 0,
+      fractionsNum: 0,
+      fractionsAddNum: 0
+    };
+  },
+  validations: {
+    leftNumerator: {
+      maxLength: maxLength(2),
+      between: between(1, 99),
+      numeric
+    },
+    leftDenominator: {
+      maxLength: maxLength(2),
+      between: between(1, 99),
+      numeric
+    },
+    rightNumerator: {
+      maxLength: maxLength(2),
+      between: between(1, 99),
+      numeric
+    },
+    rightDenominator: {
+      maxLength: maxLength(2),
+      between: between(1, 99),
+      numeric
     }
   },
   computed: {
     sum() {
-      return this.leftNumerator / this.leftDenominator + this.rightNumerator / this.rightDenominator
+      return this.leftNumerator / this.leftDenominator + this.rightNumerator / this.rightDenominator;
+    },
+    totalSum() {
+      return this.sum + this.fractionsAddNum;
     }
   },
   methods: {
-    handleValidation(val) {
-      const numbers = /^[-+]?[0-9]+$/;
-      if(val.match(numbers) && val != 0) {
-        this.isVisible = false
+    validation() {
+      if (this.$v.$anyError) {
+        this.isVisible = true;
       } else {
-        this.isVisible = true
+        this.isVisible = false;
+      }
+    },
+    addElement(min, max) {
+      if (this.numbersAmount < 5) {
+        let numRandom =
+          Math.round(min + Math.random() * (max - min)) /
+          Math.round(min + Math.random() * (max - min));
+        this.fractionsNum = Number(numRandom.toFixed(2));
+        this.fractions.push(this.fractionsNum);
+        this.numbersAmount++;
+        this.fractionsAddNum += this.fractionsNum;
+        this.isVisibleTotal = true;
+      }
+    },
+    deleteFractions(index) {
+      if (this.fractions.length > 2) {
+        let fractionsMinus = Number(this.fractions.splice(index, 1))
+        this.numbersAmount--;
+        this.fractionsAddNum -= fractionsMinus
       }
     }
   }
-}
+};
 </script>
